6369e8ede946a0876d2ae00f54534b797339fc7a
mohit <mohit.kumar@zopsmart.com>
2022-04-11 13:59:04 +0530

    remove unused query

--- store/book/query.go
+++ store/book/query.go
@@ -3,8 +3,6 @@ package book
 const (
 	getByID = `select title, author, summary, genre, year, publisher, image_uri from book where id = $1;`
 
-	getByRegNum = `select id from book where reg_num = $1;`
-
 	createBook = `insert into "book" (id, title, author, summary, genre, year, reg_num, publisher, language,
 					image_uri) values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`
 	updateBook = `update "book" set title = $1,author = $2,summary = $3,genre = $4,year = $5,
